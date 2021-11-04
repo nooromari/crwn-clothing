c4d5cd595e08e353198d0b5bbe7358db16a030b6
NOOR OMARI <nooromari847@gmail.com>
2021-11-04 10:43:40 +0200

    add header with navigation to shop page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homePage.component";
 import ShopPage from "./pages/shop/shop.component";
-
+import Header from "./components/header/header.component";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-// import "./collection-item.styles.scss";
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div calssName="collection-item">
-    <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} ></div>
-    <div className="collection-footer">
-      <span className="name">{name}</span>
-      <span className="price">{price}</span>
+  <div className='collection-item'>
+    <div
+      className='image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='collection-footer'>
+      <span className='name'>{name}</span>
+      <span className='price'>{price}</span>
     </div>
   </div>
 );
