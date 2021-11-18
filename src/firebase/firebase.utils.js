df3d36502d19ea4e3fff6351b27b4ec833b785fe
NOOR OMARI <nooromari847@gmail.com>
2021-11-18 11:00:01 +0200

    remove warnings

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,9 +3,6 @@ import { initializeApp } from "firebase/app";
 import {
   getFirestore,
   doc,
-  collection,
-  addDoc,
-  getDocs,
   getDoc,
   setDoc,
 } from "firebase/firestore";
@@ -24,6 +21,7 @@ const firebaseConfig = {
   messagingSenderId: "854206049581",
   appId: "1:854206049581:web:e41c158640502fca87b7a4",
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -62,19 +60,19 @@ export const signInWithGoogle = () =>
   signInWithPopup(auth, provider)
     .then((result) => {
       // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      // // The signed-in user info.
+      // const user = result.user;
       // ...
     })
     .catch((error) => {
       // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
+      // const errorCode = error.code;
+      // const errorMessage = error.message;
+      // // The email of the user's account used.
+      // const email = error.email;
+      // // The AuthCredential type that was used.
+      // const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
     });
