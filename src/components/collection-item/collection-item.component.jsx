cbc1cc2a13a3e647abdab7dc764bdcc0a79e9d89
NOOR OMARI <nooromari847@gmail.com>
2021-11-21 15:26:44 +0200

    remove the duplicated items from the cart and add the quantity instead

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,30 @@
 import React from "react";
+import { connect } from "react-redux";
+
+import CustomButton from "../custom-button/custom-button.component";
+import { addItem } from "../../redux/cart/cart.actions";
 
 import "./collection-item.styles.scss";
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-  <div className='collection-item'>
+const CollectionItem = ({item, addItem }) =>{ 
+  const { imageUrl, name, price } = item;
+  return(
+  <div className="collection-item">
     <div
-      className='image'
+      className="image"
       style={{
-        backgroundImage: `url(${imageUrl})`
+        backgroundImage: `url(${imageUrl})`,
       }}
     />
-    <div className='collection-footer'>
-      <span className='name'>{name}</span>
-      <span className='price'>{price}</span>
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
     </div>
+    <CustomButton onClick={() => addItem(item)} inverted > Add to Cart </CustomButton>
   </div>
-);
+)};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+export default connect(null, mapDispatchToProps)(CollectionItem);
