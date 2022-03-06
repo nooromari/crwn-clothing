3073320891de0e497d45e8956cd73e29d448310e
nooromari <nooromari847@gmail.com>
2022-03-06 16:20:01 +0300

    edit server.js for deployment

--- server.js
+++ server.js
@@ -16,13 +16,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
 
-if (process.env.NODE_ENV === 'production'){
+// if (process.env.NODE_ENV === 'production'){
     app.use(express.static(path.json(__dirname, 'client/build')));
 
     app.get('*', function(req, res) {
         res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
     });
-}
+// }
 
 app.listen(port, error => {
     if (error) throw error;
