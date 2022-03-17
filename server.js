0afe3f782cf50812a261c9a756e0069b38410a0d
nooromari <nooromari847@gmail.com>
2022-03-17 18:55:40 +0300

    for deploy

--- server.js
+++ server.js
@@ -22,9 +22,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 // if (process.env.NODE_ENV === 'production'){
     app.use(express.static(path.json(__dirname, 'client/build')));
+    app.use(express.static('client/public'))
+
 
     app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+        // res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+        app.use(express.static('client/public'))
     });
 // }
 
