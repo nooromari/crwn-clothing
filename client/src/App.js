ebd14822e7a14add06ef844c731a7eecc99a9f87
nooromari <nooromari847@gmail.com>
2022-03-30 11:05:24 +0300

    add performance optimizations

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,13 @@ import { onSnapshot } from "firebase/firestore";
 
 import { connect } from "react-redux";
 import { setCurrentUser } from "./redux/user/user.actions";
+import ErrorBoundary from "./components/error-boundary/error-boundary.component";
 
 const HomePage = lazy(() => import("./pages/homepage/homePage.component"));
 const ShopPage = lazy(() => import("./pages/shop/shop.component"));
-const SingInAndSignUp = lazy(() => import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component"));
+const SingInAndSignUp = lazy(() =>
+  import("./pages/sign-in-and-sign-up/sign-in-and-sign-up.component")
+);
 const CheckoutPage = lazy(() => import("./pages/checkout/checkout.component"));
 
 const App = ({ currentUser, setCurrentUser }) => {
@@ -42,18 +45,20 @@ const App = ({ currentUser, setCurrentUser }) => {
       <GlobalStyle />
       <Header />
       <Switch>
-        <Suspense fallback={<Spinner />}>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/shop" component={ShopPage} />
-          <Route exact path="/checkout" component={CheckoutPage} />
-          <Route
-            path="/signin"
-            exact
-            render={() =>
-              currentUser ? <Redirect to="/" /> : <SingInAndSignUp />
-            }
+        <ErrorBoundary>
+          <Suspense fallback={<Spinner />}>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
+            <Route
+              path="/signin"
+              exact
+              render={() =>
+                currentUser ? <Redirect to="/" /> : <SingInAndSignUp />
+              }
             />
-        </Suspense>
+          </Suspense>
+        </ErrorBoundary>
       </Switch>
     </div>
   );
