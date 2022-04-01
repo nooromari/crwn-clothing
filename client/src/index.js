95a522b53ff650138ba9e2eb93bd0399be0bdeda
nooromari <nooromari847@gmail.com>
2022-04-02 00:51:30 +0300

    update the project to be pwa by add service worker, https and manifest

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import { store, persistor } from "./redux/store";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 import "./index.css";
 import App from "./App";
@@ -27,3 +28,16 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+//add service worker to react app
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/service-worker.js').then(registration => {
+//       console.log('SW registered: ', registration);
+//     }).catch(registrationError => {
+//       console.log('SW registration failed: ', registrationError);
+//     });
+//   });
+// }
+
+serviceWorkerRegistration.register();
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const compression = require("compression");
 const path = require('path');
+const enforce = require('express-sslify');
 
 
 if(process.env.NODE_ENV !== 'production') require('dotenv').config();
@@ -12,13 +13,13 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
 
-
-app.use(cors());
-
 app.use(compression());
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production'){
     // app.use(express.static(path.json(__dirname, 'client/build')));
@@ -30,6 +31,10 @@ if (process.env.NODE_ENV === 'production'){
         app.use(express.static('client/build', 'index.html'))
     });
 }
+
+app.get("/service-worker.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+  });
 
 app.listen(port, error => {
     if (error) throw error;
